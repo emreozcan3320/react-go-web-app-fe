0090edcba7f1cb4eab940597e990b98d1050eaab
Emre Ozcan <emre@codeshake.io>
2020-05-17 23:27:24 +0300

    instead of local BE calls directed to GCP

--- src/services/Quote.service.js
+++ src/services/Quote.service.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL_DEV = `http://localhost:8080`
+//const API_URL_DEV = `http://localhost:8080`
 const API_URL_CLOUD = `https://golang-web-app-back-end-d6kfnlic2a-uk.a.run.app`
 
 async function listAllQuotes() {
-	 return axios (`${API_URL_DEV}/api/quotes`)
+	 return axios (`${API_URL_CLOUD}/api/quotes`)
 }
 
 async function deleteQuoteByKey(key) {
 	return axios(
 		{
 			method: 'delete',
-			url: `${API_URL_DEV}/api/quotes/${key}`
+			url: `${API_URL_CLOUD}/api/quotes/${key}`
 		})
 }
 
@@ -20,7 +20,7 @@ async function updateQuote(quoteObject) {
 	return axios(
 		{
 			method: 'PUT',
-			url: `${API_URL_DEV}/api/quotes`,
+			url: `${API_URL_CLOUD}/api/quotes`,
 			data: {
 				key: quoteObject.key,
 				quote: quoteObject.quote,
@@ -35,7 +35,7 @@ async function createQuote(quoteObject) {
 	return axios(
 		{
 			method: 'post',
-			url: `${API_URL_DEV}/api/quotes`,
+			url: `${API_URL_CLOUD}/api/quotes`,
 			data: {
 				quote: quoteObject.quote,
 				owner: quoteObject.owner,
